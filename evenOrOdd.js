4a43bf1f5a0636346925eb804621b8f9ad411179
Xolani-M <Nathanielmvana@gmail.com>
2021-07-21 07:09:44 +1200

    repleced return with print

--- evenOrOdd.js
+++ evenOrOdd.js
@@ -1,7 +1,7 @@
 function evenOrOdd(int) {
-  if (int % 2 === 0) {
-    return "even";
-  } else
-return "odd";
-}
-console.log(evenOrOdd(3)); 
+    if (int % 2 === 0) {
+      print("even");
+    } else
+  print("odd");
+  }
+  console.log(evenOrOdd(3));
