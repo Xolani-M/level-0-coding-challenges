b6974bfbb959da43691cbbceb8ba9147995dfdfb
Xolani-M <Nathanielmvana@gmail.com>
2021-07-21 09:28:24 +1200

    the output is defined

--- printOutVowels.js
+++ printOutVowels.js
@@ -1,14 +1,11 @@
-function printOutVowels(v){
-    var str=""; 
-    for(var i = 0; i < v.length ; i++);
+function ptintOutVowels(v){
+    let str="";
+    for(var i=0; i<v.length ; i++)
     {
-        if((v[i] === "a") || (v[i] === "e") || (v[i] === "i")||(v[i] === "o")||(v[i] === "u")){
+if((v[i]==="a")||(v[i]==="e")||(v[i]==="i")||(v[i]==="o")||(v[i]==="u")){
             console.log(v[i]);
         }
-        else{
-            str=str.concat(v[i]).concat("\n");
-        }
     }
-    console.log(str);
+    console.log(str)
 }
-printOutVowels("Umuzi");
+ptintOutVowels("Umuzi")
