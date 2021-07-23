647f2dd2e6c2266905554cc81c613eb8df948553
Xolani-M <Nathanielmvana@gmail.com>
2021-07-23 13:51:49 +1200

    time converter fixed

--- timeConverter.js
+++ timeConverter.js
@@ -1,12 +1,10 @@
 function timeConvert(number) {
-var num = number;
+var num = number
 var hours = (num / 60);
 var hrs = Math.floor(hours);
 var minutes = (hours - hrs) * 60;
 var min = Math.round(minutes);
-return num + " = " + hrs + " hours, " + min + " minutes.";
+return + hrs + " hours, " + min + " minutes.";
 }
 
 console.log(timeConvert(133));
-
-
