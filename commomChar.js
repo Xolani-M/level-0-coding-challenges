a46f7b7f203740b070e057158629855253022a0f
Xolani-M <Nathanielmvana@gmail.com>
2021-07-23 21:58:33 +1200

    function prints common chars and does not duplicate, had to remove a bug too

--- commomChar.js
+++ commomChar.js
@@ -5,7 +5,7 @@ var s2 ="computers";
   for(var i=0; i < s1.length; i++) {
    dict[s1.charAt(i)] = 1;
 } var commonChars =[];
-  for(var i=0; i < s2.length; i++) {
+  for(i = 0; i < s2.length; i++) {
    if( dict[s2.charAt(i)] === 1) {
    commonChars.push(s2.charAt(i));
 }
@@ -15,3 +15,4 @@ console.log(dict);
 }
 
 commonChar();
+
