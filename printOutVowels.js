a27d3008b6cfa5ca6a630b2f648707ad6854ac3b
Xolani-M <Nathanielmvana@gmail.com>
2021-07-25 18:13:30 +1200

    fixed function to the required standard

---
+++ printOutVowels.js
@@ -0,0 +1,11 @@
+function ptintOutVowels(v){
+    let str="";
+    for(var i=0; i<v.length ; i++)
+    {
+if((v[i]==="a")||(v[i]==="e")||(v[i]==="i")||(v[i]==="o")||(v[i]==="u")){
+            console.log(v[i]);
+        }
+    }
+    console.log(str)
+}
+ptintOutVowels("Umuzi")
