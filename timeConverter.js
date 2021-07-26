6a9205772459f6750a503ba66eb7a4f6f84a0457
Xolani-M <Nathanielmvana@gmail.com>
2021-07-26 12:30:53 +1200

    funtion takes both singular and plural numbers

--- timeConverter.js
+++ timeConverter.js
@@ -1,10 +1,10 @@
-function timeConvert(number) {
-var num = number
-var hours = (num / 60);
-var hrs = Math.floor(hours);
-var minutes = (hours - hrs) * 60;
-var min = Math.round(minutes);
-return + hrs + " hours, " + min + " minutes.";
+function timeConverter(number) {
+  var num = number
+  var hours = (num / 60);
+  var hrs = Math.floor(hours);
+  var minutes = (hours - hrs) * 60;
+  var min = Math.round(minutes);
+  return + hrs + (hrs === 1 ? " hour, " : " hours, ") + min + (min === 1 ? " minute." : " minutes.")
 }
 
-console.log(timeConvert(133));
+console.log(timeConverter(111));
