5027fbc42b331ec55accbfa4da792d5ae6c6fb13
Xolani-M <Nathanielmvana@gmail.com>
2021-09-09 17:58:18 +1200

    vowels do not duplicate

--- printOutVowels.js
+++ printOutVowels.js
@@ -1,13 +1,11 @@
-function printOutVowels (str){
-var vowels = ["a", "e", "i", "o", "u"]
-var letters = str.split("");
-var vowelsFound = [];
-   for (var i in letters) {
-        if (vowels.includes(letters[i])) {
-            vowelsFound.push(letters[i]);
+function printOutVowels(str){
+    let vowels = "aeiou";
+    let vowelsFound = "";
+    for(let i = 0; i < vowels.length; i++){
+        if(str.includes(vowels[i])){
+          vowelsFound += vowels[i] + " ";
         }
     }
-    console.log("Vowels:", vowelsFound.join(", "));    
+    console.log("Vowels:", vowelsFound);
 }
-
-printOutVowels(("Umuzi"));
+printOutVowels("UmuziAcademy");
