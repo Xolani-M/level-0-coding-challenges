cb506c465568f627f309d174fd06c667f673c958
Xolani-M <Nathanielmvana@gmail.com>
2021-09-19 16:57:10 +1200

    eighth task

--- timeConverter.js
+++ timeConverter.js
@@ -1,10 +1,10 @@
-function timeConverter(number) {
-  var num = number
-  var hours = (num / 60);
-  var hrs = Math.floor(hours);
-  var minutes = (hours - hrs) * 60;
-  var min = Math.round(minutes);
-  return + hrs + (hrs === 1 ? " hour, " : " hours, ") + min + (min === 1 ? " minute." : " minutes.")
-}
+let timeConverters =(num) => {
+    let hours = (num / 60);
+    let hrs = Math.floor(hours);
+    let minutes = (hours - hrs) * 60;
+    let min = Math.round(minutes);
+    return hrs + (hrs === 1 ? " hour, " : " hours, ") + min + (min === 1 ? " minute." : " minutes.");
+  }
+  
+  console.log(timeConverters(133));
 
-console.log(timeConverter(111));
