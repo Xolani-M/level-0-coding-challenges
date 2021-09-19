e7be61242a1754d8f39f08cf944bc0ecbf33676d
Xolani-M <Nathanielmvana@gmail.com>
2021-09-19 17:02:14 +1200

    ninth task

--- printOutVowels.js
+++ printOutVowels.js
@@ -1,11 +1,11 @@
-function printOutVowels(str){
+let printOutVowels = (str) =>{
     let vowels = "aeiou";
     let vowelsFound = "";
     for(let i = 0; i < vowels.length; i++){
         if(str.includes(vowels[i])){
-          vowelsFound += vowels[i] + " ";
+          vowelsFound += vowels[i];
         }
     }
-    console.log("Vowels:", vowelsFound);
-}
+    console.log("Vowels: " + Array.prototype.join.call(vowelsFound));
+};
 printOutVowels("UmuziAcademy");
