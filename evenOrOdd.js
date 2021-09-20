0da27fd6e3ab4be84db7d4eef6ec831eeaba861a
Xolani-M <Nathanielmvana@gmail.com>
2021-09-20 12:42:28 +1200

    printing instead of return

--- evenOrOdd.js
+++ evenOrOdd.js
@@ -1,4 +1,10 @@
-let evenOrOdd = (int) => int % 2 === 0 ? "even" : "odd"; 
-console.log(evenOrOdd(7));
+let evenOrOdd = (int) => {
+  if(int % 2 === 0){
+    console.log("even");
+  }else{
+    console.log("odd");
+  }
+}
+(evenOrOdd(7));
 
 
