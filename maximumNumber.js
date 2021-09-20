fea63cf0fa350b36897f8879ef7d46df8bac75d5
Xolani-M <Nathanielmvana@gmail.com>
2021-09-20 12:59:38 +1200

    returning instead of printing

--- maximumNumber.js
+++ maximumNumber.js
@@ -1,10 +1,10 @@
-function maximumNumber(...array){
+let maximumNumber = (...array) =>{
     let max = 0;
     for(let i = 0; i < array.length; i++){
         if(array[i] > max){
             max = array[i];
-        } 
+        }
     }
-    console.log(max);
+    return max;
 }
-maximumNumber(1,66,35,455,56,8,66);
+console.log(maximumNumber(1,66,35,455,56,8,66));
