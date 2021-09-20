5fffe2c17e02dfc040039c8ee93873890a502f14
Xolani-M <Nathanielmvana@gmail.com>
2021-09-20 12:54:26 +1200

    returning instead of printing

--- equilTriangle.js
+++
@@ -1,5 +0,0 @@
-let equilateralTriangle = (x,y,z) =>{
-    let area = (x + y + z) / 2; 
-    console.log(Math.sqrt(area * (area - x) * (area - y) * (area - z)));
-}
-equilateralTriangle(6,6,6);
--- equilateralTriangle.js
+++ equilateralTriangle.js
@@ -1,5 +1,6 @@
 let equilateralTriangle = (x,y,z) =>{
-    let area = (x + y + z) / 2; 
-    console.log(Math.sqrt(area * (area - x) * (area - y) * (area - z)));
+    let area = (x + y + z) / 2;
+    return Math.sqrt(area * (area - x) * (area - y) * (area - z));
 }
-equilateralTriangle(6,6,6);
+console.log(equilateralTriangle(6,6,6));
+
